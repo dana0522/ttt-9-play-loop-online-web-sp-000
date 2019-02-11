54a8caf13bef7d94aaf177911955bc5e593e9831
dana kim <dana0522@live.com>
2019-02-11 01:55:48 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -31,7 +31,12 @@ def turn(board)
     move(board, index)
     display_board(board)
   else
-    turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
   end
 end
 
